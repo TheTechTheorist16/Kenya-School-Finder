8c302a14fcaf7e2206fabf8039a2acc073d554bf
TheTechTheorist16 <brianeggman.2131@gmail.com>
2026-07-30 14:09:00 +0300

    Enable CORS for Vercel

--- Backend/Main.py
+++ Backend/Main.py
@@ -16,7 +16,15 @@ app = FastAPI(
 # -------------------------------
 # CORS
 # -------------------------------
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://kenya-school-finder.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
